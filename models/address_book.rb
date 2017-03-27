941404da0c1089ba379882cd3867dbd0d7038c6b
Brandon Mills <sllimnodnarb@gmail.com>
2017-03-27 12:34:53 -0500

    adding update to address_bloc.rb

--- models/address_book.rb
+++ models/address_book.rb
@@ -13,7 +13,7 @@ class AddressBook
       if name < entry.name
         break
       end
-      index+= 1
+      index += 1
     end
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
