8a4bc8ad031514e673affe72c7269ff6a0966a7b
Brandon Mills <sllimnodnarb@gmail.com>
2017-03-26 20:52:29 -0500

    adding modifications to address-bloc-models

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,24 +8,23 @@ class AddressBook
     end
 
   def add_entry(name, phone_number, email)
-    # #9
     index = 0
     @entries.each do |entry|
-    # #10
       if name < entry.name
         break
       end
       index+= 1
     end
-    # #11
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
 
   def remove_entry(name, phone_number, email)
+    selected = nil
     @entries.each do |entry|
       if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        selected = entry
       end
-      @entries.delete(entry)
+      @entries.delete(selected)
     end
   end
 end
