ca23e1e1cb5576bf72023701c1dd6df9f52b9836
Brandon Mills <sllimnodnarb@gmail.com>
2017-03-28 15:38:22 -0500

    adding files associated with assignment 6 menu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View Entry Number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -24,17 +25,21 @@ class MenuController
          main_menu
        when 2
          system "clear"
-         create_entry
+         view_entry_number
          main_menu
        when 3
          system "clear"
-         search_entries
+         create_entry
          main_menu
        when 4
          system "clear"
-         read_csv
+         search_entries
          main_menu
        when 5
+         system "clear"
+         read_csv
+         main_menu
+       when 6
          puts "Good-bye!"
          exit(0)
        else
@@ -53,6 +58,19 @@ class MenuController
 
     system "clear"
     puts "End of entries"
+   end
+
+   def view_entry_number
+     puts "What entry number do you want to search for?"
+     n = (gets.chomp.to_i) 
+     if n < @address_book.entries.count && n > 0
+       puts @address_book.entries[n]
+       puts "Press enter to view main menu"
+       gets.chomp
+       system "clear"
+     else
+       puts "#{n} not found, try a lower number"
+     end
    end
 
    def create_entry
